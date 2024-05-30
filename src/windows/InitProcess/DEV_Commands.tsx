408b08765321f81b57f744baf1ed2f3b320e553c
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2024-05-30 23:02:39 +0400

    Bump channel force closure

--- src/windows/InitProcess/DEV_Commands.tsx
+++ src/windows/InitProcess/DEV_Commands.tsx
@@ -45,7 +45,7 @@ import {
   getTransaction,
   getTransactions,
 } from "../../storage/database/transaction";
-import { genSeed, initWallet, signMessage } from "../../lndmobile/wallet";
+import { genSeed, initWallet, pendingSweeps, signMessage } from "../../lndmobile/wallet";
 import { getPin, getWalletPassword } from "../../storage/keystore";
 import { lnrpc, routerrpc } from "../../../proto/lightning";
 import { modifyStatus, queryScores, status } from "../../lndmobile/autopilot";
@@ -71,6 +71,7 @@ import { blixtTheme } from "../../native-base-theme/variables/commonColor";
 import { generateSecureRandom } from "react-native-securerandom";
 import { localNotification } from "../../utils/push-notification";
 import { sendCommand } from "../../lndmobile/utils";
+import { getCFilter } from "../../lndmobile/neutrino";
 
 let iCloudStorage: any;
 console.log(PLATFORM);
@@ -145,6 +146,17 @@ export default function DEV_Commands({ navigation, continueCallback }: IProps) {
             </>
           )}
           <Text style={{ width: "100%" }}>Random:</Text>
+          <Button
+            small
+            onPress={async () => {
+              Alert.prompt("Compact filter", undefined, async (hash) => {
+                const filter = await getCFilter(hash);
+                Alert.alert(filter.filter.byteLength.toString());
+              });
+            }}
+          >
+            <Text style={styles.buttonText}>getCFilter size</Text>
+          </Button>
           <Button
             small
             onPress={async () => {
@@ -1282,6 +1294,7 @@ export default function DEV_Commands({ navigation, continueCallback }: IProps) {
             ["getNetworkInfo", getNetworkInfo],
             ["getTransactions", getTransactionsOnchain],
             ["closedChannels", closedChannels],
+            ["pendingSweeps", pendingSweeps],
           ].map(([name, f], i) => {
             return (
               <Button
